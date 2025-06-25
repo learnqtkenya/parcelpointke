4ac695fe3212f6fbc57893cffb43114ecb93e631
EricoDeMecha <ford.koech.eric@gmail.com>
2025-06-25 21:10:14 +0300

    (main): prep deployment

--- next.config.ts
+++ next.config.ts
@@ -1,20 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
-  trailingSlash: true,
-  skipTrailingSlashRedirect: true,
-  
   images: {
     unoptimized: true,
   },
-  
-  // basePath: '/parcelpointke',
-  // assetPrefix: '/parcelpointke',
-  
-  compress: true,
-  
-  poweredByHeader: false,
-
 }
 
 module.exports = nextConfig
